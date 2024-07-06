655cdae8fc671f9f433d85888bbbc7fb7bede5cc
Travis <travis@mooremediallc.com>
2024-07-05 20:11:35 -0700

    added search

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -57,6 +57,38 @@ const config: Config = {
   ],
 
   themeConfig: {
+    algolia: {
+      // The application ID provided by Algolia
+      appId: 'DTCGWK1H6T',
+
+      // Public API key: it is safe to commit it
+      apiKey: 'c72eed8af1c2e0b9e1839650445eacb4',
+
+      indexName: 'alpacastack',
+
+      // Optional: see doc section below
+      contextualSearch: true,
+
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      externalUrlRegex: 'external\\.com|domain\\.com',
+
+      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+      replaceSearchResultPathname: {
+        from: '/docs/', // or as RegExp: /\/docs\//
+        to: '/',
+      },
+
+      // Optional: Algolia search parameters
+      searchParameters: {},
+
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: 'search',
+
+      // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+      insights: false,
+
+      //... other Algolia params
+    },
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     docs: {
